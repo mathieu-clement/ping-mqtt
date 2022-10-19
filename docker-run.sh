3067087f8c6cd626e52a2e7503e5fb3af627e235
Mathieu Clement <mathieu.clement@fairtiq.com>
2022-10-19 23:36:23 +0200

    update targets

--- docker-run.sh
+++ docker-run.sh
@@ -2,7 +2,7 @@
 set -eu
 
 docker run \
-    -e 'TARGETS=192.168.0.2,192.168.0.254' \
+    -e 'TARGETS=192.168.0.11,192.168.0.14' \
     -e 'BROKER=192.168.0.2' \
     -e 'POLLING_PERIOD=5' \
     -e 'LOG_LEVEL=DEBUG' \
