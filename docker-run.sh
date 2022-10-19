1313676c0f84326e9a5239d4c2ae8172351a5306
Mathieu Clement <mathieu.clement@fairtiq.com>
2022-10-19 22:50:08 +0200

    Change env variables so there's some output to see

--- docker-run.sh
+++ docker-run.sh
@@ -4,7 +4,8 @@ set -eu
 docker run \
     -e 'TARGETS=192.168.0.2,192.168.0.254' \
     -e 'BROKER=192.168.0.2' \
-    -e 'POLLING_PERIOD=60' \
+    -e 'POLLING_PERIOD=5' \
+    -e 'LOG_LEVEL=DEBUG' \
     --rm \
     --name ping-mqtt \
     mathieuclement/ping-mqtt:latest
